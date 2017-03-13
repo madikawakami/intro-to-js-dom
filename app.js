5f19f78a21e922c2009e5644aaba75e6a8ffd3b7
Madison Kawakami <madi.kawakami@gmail.com>
2017-03-13 08:30:10 -1000

    sup

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 console.log("Sanity check");
+
+//Finding HTML elements
+console.log(document.getElementById("use-id-method"));
+var idElement = document.getElementById("use-id-method");
+console.log(idElement);
+
+//finding elements by tag name
+console.log(document.getElementsByTagName("script"));
+var TagName = document.getElementsByTagName("script");
+console.log(TagName);
