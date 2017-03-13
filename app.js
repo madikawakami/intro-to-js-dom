62d3f70897cf4b5cd34ab3654b76eb5f76c1b5a4
Madison Kawakami <madi.kawakami@gmail.com>
2017-03-13 08:40:53 -1000

    new

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 console.log("Sanity check");
 
 //Finding HTML elements
-console.log(document.getElementById("use-id-method"));
+//console.log(document.getElementById("use-id-method"));
 var idElement = document.getElementById("use-id-method");
 console.log(idElement);
 
 //finding elements by tag name
-console.log(document.getElementsByTagName("script"));
-var TagName = document.getElementsByTagName("script");
-console.log(TagName);
+//console.log(document.getElementsByTagName("script"));
+var TagElements = document.getElementsByTagName("script");
+console.log(TagElements);
+
+//finding elements by class name
+//console.log(document.getElementsByClassName("use-class-method"));
+var	ClassElements = document.getElementsByClassName("use-class-method");
+console.log(ClassElements);
+
+//changing HTML elements
+console.log(idElement.innerHTML);
+idElement.innerHTML = "I Have Been Changed";
